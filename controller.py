13c46a4c631442f583fb8bf5e5e5f687fd28c6a5
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2023-08-10 14:30:29 -0600

    DB conection and 'general class' added

--- controller.py
+++ controller.py
@@ -4,19 +4,36 @@
 '''
 This file consists of the definition of the Controller class wich is in charge of handling the requests and responses of the API.
 '''
-from flask import Flask, request
 
+from flask import Flask, request, jsonify
+from models import general
+from flask_cors import CORS
 
 class Controller():
 
     def __init__(self):
         self.app = Flask(__name__)
-        # CORS(self.app, resources={r"/*": {"origins": "*"}})
-        self.app.route('/')(self.test)
+        self.general = general.General()
+        CORS(self.app, resources={r"/*": {"origins": "*"}})
+        
+        # Routes
+        self.app.route('/test')(self.test)
+        self.app.route('/getAllGeneral', methods=['GET'])(self.getAllGeneral)
+        self.app.route('/addGeneral', methods=['POST'])(self.addGeneral)
 
     def test(self):
         print("Hello World!")
         return "Hello World!"
+    
+    def getAllGeneral(self):
+        return jsonify(self.general.find({})), 200
+    
+    def addGeneral(self):
+        if request.method == "POST":
+            title = request.get_json()['title']
+            body = request.get_json()['body']
+            response = self.general.create({'title': title, 'body': body})
+            return response, 201
     
     def run(self):
         self.app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ models/general.py
@@ -0,0 +1,54 @@
+# Final Project: Web Application
+# Authors:
+#          A01746664 Eduardo Joel Cortez Valente
+'''
+This file consists of the definition of the General class wich is in charge of handling the requests and responses of the API.
+'''
+
+from factory.validation import Validator
+from factory.database import Database
+
+class General(object):
+    def __init__(self):
+        self.validator = Validator()
+        self.db = Database()
+        
+        self.collection_name = "test"
+        
+        self.fields = {
+            "name": "string",
+            "body": "string",
+            "created": "datetime",
+            "updated": "datetime"
+        }
+        
+        self.create_required_fields = ["title", "body"]
+
+        # Fields optional for CREATE
+        self.create_optional_fields = []
+
+        # Fields required for UPDATE
+        self.update_required_fields = ["title", "body"]
+
+        # Fields optional for UPDATE
+        self.update_optional_fields = []
+        
+    def create(self, todo):
+        # Validator will throw error if invalid
+        self.validator.validate(todo, self.fields, self.create_required_fields, self.create_optional_fields)
+        res = self.db.insert(todo, self.collection_name)
+        return "Inserted Id " + res
+
+    def find(self, todo):  # find all
+        return self.db.find(todo, self.collection_name)
+
+    def find_by_id(self, id):
+        return self.db.find_by_id(id, self.collection_name)
+
+    def update(self, id, todo):
+        self.validator.validate(todo, self.fields, self.update_required_fields, self.update_optional_fields)
+        return self.db.update(id, todo,self.collection_name)
+
+    def delete(self, id):
+        return self.db.delete(id, self.collection_name)
+        
